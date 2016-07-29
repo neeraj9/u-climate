18d37d26ff4905a3bc781974f99f80ff6e0e7568
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2016-07-29 23:54:02 +0530

    query weather information for user provided latitude and longitude instead of getting it from some hard-coded value, which was primarily used for testing.

--- src/uclimate_geocircle_handler.erl
+++ src/uclimate_geocircle_handler.erl
@@ -45,6 +45,13 @@
 %% Custom callbacks
 -export([json_text/2]).
 
+%% Set the openweather.org api key here
+%% TODO move out of code and let it be configured at runtime instead.
+-define(API_KEY, "<API_KEY_HERE>").
+-define(MAXIMUM_CITIES, 20).
+-define(DEFAULT_MAXIMUM_CITIES_BIN, <<"10">>).
+-define(THIRD_PARTY_GEO_FLOAT_PRECISION, 1).
+
 
 init(Req, Opts) ->
   {cowboy_rest, Req, Opts}.
@@ -57,11 +64,37 @@ content_types_provided(Req, State) ->
     {<<"application/json">>, json_text}
   ], Req, State}.
 
+%% Need latitude and longitude as query string within the
+%% request otherwise this API will fail and the process will
+%% crash, so cowboy will return HTTP/1.1 500 Internal Server Error.
+%% TODO the HTTP 500 error code is inappropriate for errors
+%% where the user provided incorrect URL or missing options.
+%% Instead return bad request http code.
+-spec(json_text(Req, State) ->
+  {ResponseBody, Req, State}).
 json_text(Req, State) ->
+  #{lat:=LatVal, lon:=LongVal, cnt:=CntVal} =
+    cowboy_req:match_qs([lat, lon, {cnt, [], ?DEFAULT_MAXIMUM_CITIES_BIN}],
+      Req),
+  % lets try to convert them for validation rather than pass
+  % to third-party site as-is and fail there unnecessarily.
+  GeoLat = safe_binary_to_float(LatVal),
+  GeoLong = safe_binary_to_float(LongVal),
+  CityCount = safe_binary_to_float(CntVal),
+  true = GeoLat > -91 andalso GeoLat < 91,
+  true = GeoLong > -181 andalso GeoLong < 181,
+  true = CityCount > 0 andalso CityCount =< ?MAXIMUM_CITIES,
+  ValidatedLatVal = float_to_list(GeoLat,
+    [{decimals, ?THIRD_PARTY_GEO_FLOAT_PRECISION}]),
+  ValidatedLongVal = float_to_list(GeoLong,
+    [{decimals, ?THIRD_PARTY_GEO_FLOAT_PRECISION}]),
   % The /find api looks for latitude and longitude and find 10 (cnt=10)
   % cities for weather information.
   % see http://openweathermap.org/current for more details.
-  Url = "http://api.openweathermap.org/data/2.5/find?lat=12.9&lon=77.5&cnt=10&appid=<API_KEY_HERE>",
+  Url = "http://api.openweathermap.org/data/2.5/find?lat=" ++
+    ValidatedLatVal ++ "&lon=" ++ ValidatedLongVal ++
+    "&cnt=10&appid=" ++ ?API_KEY,
+  lager:debug("fetching Url=~s~n", [Url]),
   WeatherResponse = httpc:request(Url),
   case WeatherResponse of
     {ok, ResponseContents} ->
@@ -71,3 +104,17 @@ json_text(Req, State) ->
       ResponseBody = <<"{}">>
   end,
   {ResponseBody, Req, State}.
+
+
+safe_binary_to_float(Bin) ->
+  NumString = binary_to_list(Bin),
+  case string:to_float(NumString) of
+    {error, no_float} ->
+      case string:to_integer(NumString) of
+        {error, no_integer} ->
+          error;
+        % convert integer to float
+        {IntVal, _Rest} -> IntVal / 1.0
+      end;
+    {Val, _Rest} -> Val
+  end.
